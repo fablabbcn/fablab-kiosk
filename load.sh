813abc7fbf41c9a2699404e2f5878ee09994458b
vicobarberan <victor@smartcitizen.me>
2018-05-15 09:29:15 +0000

    testing

--- load.sh
+++ load.sh
@@ -3,7 +3,7 @@ BASE="/home/pi/fablab-kiosk/";
 VP="viewport_";
 NUM="1"
 
-cd $BASE && git pull;
+sleep 10 && cd $BASE && git pull;
 
 i3-msg "workspace $NUM; append_layout $BASE$VP$NUM/ws1.json"
 (surf $BASE$VP$NUM"/index.html" &)
